a45f174635858a48748b41a323bcdd1ac33bc01d
SokKimThanh <thanhsk1991@gmail.com>
2024-08-01 10:32:14 +0700

    chỉnh vị trí navigation các màn hình

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,10 @@ $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
+    navText: [
+        '<i class="bi bi-arrow-left" aria-hidden="true"></i>',
+        '<i class="bi bi-arrow-right" aria-hidden="true"></i>'
+    ],
     responsive: {
         0: {
             items: 1
