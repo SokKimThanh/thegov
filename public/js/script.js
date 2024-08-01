185a4105ca25aa21d27c66477f251e0ee0ce94d0
SokKimThanh <thanhsk1991@gmail.com>
2024-08-01 16:19:46 +0700

    thêm tắt mở ô search

--- public/js/script.js
+++ public/js/script.js
@@ -18,3 +18,38 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+/**
+ * Tạo kiểu cho search-container
+ */
+
+
+const searchContainer = document.querySelector('.search-container');
+const searchArea = searchContainer.querySelector('.search-area');
+const searchIcon = searchContainer.querySelector('.search-icon');
+const searchX = searchArea.querySelector('.btn-x');
+
+searchIcon.addEventListener('click', () => {
+    const searchInput = searchArea.querySelector('.search-input');
+    searchX.addEventListener('click', () => {
+        searchArea.classList.remove('show');
+        searchIcon.classList.remove('active');
+        searchX.classList.remove('active');
+    });
+    searchArea.classList.add('show');
+    searchIcon.classList.add('active');
+    searchX.classList.add('active');
+    searchInput.addEventListener('focus', () => {
+        searchInput.removeAttribute('placeholder');
+    });
+    searchInput.addEventListener('blur', () => {
+        searchInput.setAttribute('placeholder', 'Tìm kiếm');
+    });
+});
+
+const sliderRevolution = document.querySelector('#slider-revolution');
+sliderRevolution.addEventListener('click', () => {
+    if (searchX) {
+        searchX.click();
+    }
+}) 
