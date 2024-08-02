33e816245c549eb8e891dc4624a2e6cb37f6246a
SokKimThanh <thanhsk1991@gmail.com>
2024-08-02 17:24:06 +0700

    báo cáo thực tập buổi 30

--- public/js/script.js
+++ public/js/script.js
@@ -52,4 +52,77 @@ sliderRevolution.addEventListener('click', () => {
     if (searchX) {
         searchX.click();
     }
-}) 
+})
+
+// MENU DESKTOP
+/**
+ * JavaScript Functionality
+JavaScript thêm các hành vi động cho menu:
+
+Xử lý sự kiện click:
+
+Khi click vào một menu-item, các menu-item khác sẽ loại bỏ class active, 
+và mục được click sẽ thêm class active.
+Nếu menu-item có menu con (sub-menu), menu con sẽ được hiển thị.
+Xử lý sự kiện hover:
+
+Khi hover vào một mục có menu con, menu con sẽ được hiển thị 
+(subMenu.classList.add('show')).
+Khi di chuyển chuột ra khỏi mục, menu con sẽ bị ẩn 
+(subMenu.classList.remove('show')).
+Kiểm tra và xử lý các mục không có submenu:
+
+Nếu một menu-item không có submenu, nó sẽ được thêm class no-children, 
+và mũi tên chỉ xuống (.menu-link::after) sẽ bị ẩn.
+ */
+const menuItems = document.querySelectorAll('.menu-item');
+
+menuItems.forEach(item => {
+    // Kiểm tra nếu menu-item không có menu con
+    const subMenu = item.querySelector('.sub-menu');
+    // Tìm phần tử overlay
+
+
+    // Nếu menu-item không có menu con, thêm class no-children vào nó
+    if (!subMenu) {
+        item.classList.add('no-children');
+    }
+
+    // Xử lý sự kiện click
+    item.addEventListener('click', (e) => {
+        // Ngăn chặn sự kiện click lan truyền lên các phần tử cha
+        e.stopPropagation();
+
+        // Loại bỏ lớp active từ tất cả menu items
+        menuItems.forEach(menuItem => {
+            menuItem.classList.remove('active');
+        });
+
+        // Thêm lớp active cho menu item được click
+        item.classList.add('active');
+
+        // Nếu mục menu cha có sub-menu, ngăn không đóng nó
+        if (subMenu) {
+            subMenu.classList.add('show');
+        }
+
+        // Kiểm tra xem menu-item có cha không và thêm lớp active cho nó
+        let parentItem = item.closest('.menu-item');
+        if (parentItem) {
+            parentItem.classList.add('active');
+        }
+    });
+
+    // Xử lý sự kiện hover để hiển thị sub menu
+    item.addEventListener('mouseenter', () => {
+        if (subMenu) {
+            subMenu.classList.add('show');
+        }
+    });
+
+    item.addEventListener('mouseleave', () => {
+        if (subMenu) {
+            subMenu.classList.remove('show');
+        }
+    });
+});
